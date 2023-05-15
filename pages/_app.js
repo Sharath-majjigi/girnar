4d30bd0e9964abbf86d05c20a6d75e8f3bbf742b
yaswanthmyneni <yaswanthmyneni@gmail.com>
2023-05-15 19:53:30 +0530

    fix deploy warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
 import "/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
   const r = useRouter();
