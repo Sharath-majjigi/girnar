357b891d72317316a3bc4eb735ed623f303344b3
yaswanthmyneni <yaswanthmyneni@gmail.com>
2023-05-15 19:23:52 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   const r = useRouter();
-  
+
   return (
     <>
       <Head>
