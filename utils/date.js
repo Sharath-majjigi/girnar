0abbd99b70fe152747e54364c92589dbe5606767
Sharath <sharathholmes143@gmail.com>
2023-07-02 20:40:53 +0530

    Changed Date-format

--- utils/date.js
+++ utils/date.js
@@ -1,8 +1,11 @@
 const getDateFormate = () => {
   const date = new Date();
-  const dateString = date.toLocaleDateString().split("/");
-  const dateFormate = `${dateString[2]}-${dateString[1]}-${dateString[0]}`;
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const dateFormate = `${year}-${month}-${day}`;
   return dateFormate;
 };
 
 export { getDateFormate };
+
